1c5d586ed90e978f6f57bf970af0a73475576926
Gustavo Castro <gustavoprasca@hotmail.com>
2025-01-10 16:38:30 -0500

    migration from springfox to springdoc openAPI

--- src/main/java/com/co/softworld/credibanco/controller/impl/CustomerControllerImpl.java
+++ src/main/java/com/co/softworld/credibanco/controller/impl/CustomerControllerImpl.java
@@ -3,8 +3,8 @@ package com.co.softworld.credibanco.controller.impl;
 import com.co.softworld.credibanco.controller.ICustomerController;
 import com.co.softworld.credibanco.model.Customer;
 import com.co.softworld.credibanco.service.ICustomerService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,9 +12,9 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/customer")
+@RequestMapping("/${api.version}/customer")
 @AllArgsConstructor
-@Api(tags = "Customer Controller")
+@Tag(name = "Customer Controller")
 public class CustomerControllerImpl implements ICustomerController {
 
     private final ICustomerService customerService;
@@ -22,7 +22,7 @@ public class CustomerControllerImpl implements ICustomerController {
 
     @Override
     @PostMapping("/create")
-    @ApiOperation(value = "Registra el cliente. Recibe en el cuerpo del mensaje el username, password sin encriptar y " +
+    @Operation(summary = "Registra el cliente. Recibe en el cuerpo del mensaje el username, password sin encriptar y " +
             "los roles asociados (Deben iniciar con la palabra ROLE_) separados por coma.")
     public ResponseEntity<Customer> create(@RequestBody Customer customer) {
         return customerService.create(customer);
@@ -30,7 +30,7 @@ public class CustomerControllerImpl implements ICustomerController {
 
     @Override
     @GetMapping
-    @ApiOperation(value = "Consulta los cliente registrados.")
+    @Operation(summary = "Consulta los cliente registrados.")
     public ResponseEntity<List<Customer>> findAll() {
         return customerService.findAll();
     }
--- src/main/java/com/co/softworld/credibanco/controller/impl/TransactionControllerImpl.java
+++ src/main/java/com/co/softworld/credibanco/controller/impl/TransactionControllerImpl.java
@@ -4,8 +4,8 @@ import com.co.softworld.credibanco.controller.ITransactionController;
 import com.co.softworld.credibanco.model.TransactionManager;
 import com.co.softworld.credibanco.model.TransactionMapper;
 import com.co.softworld.credibanco.service.ITransactionService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,8 +13,8 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/transaction")
-@Api(tags = "Transaction Controller")
+@RequestMapping("/${api.version}/transaction")
+@Tag(name = "Transaction Controller")
 @AllArgsConstructor
 public class TransactionControllerImpl implements ITransactionController {
 
@@ -22,7 +22,7 @@ public class TransactionControllerImpl implements ITransactionController {
 
     @Override
     @PostMapping("/purchase")
-    @ApiOperation(value = "Realiza una compra. Recibe en el cuerpo del mensaje el id de la tarjeta y el precio de la " +
+    @Operation(summary = "Realiza una compra. Recibe en el cuerpo del mensaje el id de la tarjeta y el precio de la " +
             "compra, valida que la tarjeta exista, que se encuentre activa, que la fecha de expiración esté vigente, " +
             "que el precio de la compra sea mayor a 0 y tenga el saldo suficiente para procesar la transacción.")
     public ResponseEntity<TransactionManager> purchase(@RequestBody TransactionMapper mapper) {
@@ -31,7 +31,7 @@ public class TransactionControllerImpl implements ITransactionController {
 
     @Override
     @GetMapping("/{transactionId}")
-    @ApiOperation(value = "Consulta una transacción. Recibe por parámetro el id de la transacción, valida que la " +
+    @Operation(summary = "Consulta una transacción. Recibe por parámetro el id de la transacción, valida que la " +
             "transacción exista y genera la consulta.")
     public ResponseEntity<TransactionManager> getPurchase(@PathVariable int transactionId) {
         return transactionService.getPurchase(transactionId);
@@ -39,7 +39,7 @@ public class TransactionControllerImpl implements ITransactionController {
 
     @Override
     @PostMapping("/anulation")
-    @ApiOperation(value = "Anula una transacción. Recibe en el cuerpo del mensaje el id de la transacción, valida que " +
+    @Operation(summary = "Anula una transacción. Recibe en el cuerpo del mensaje el id de la transacción, valida que " +
             "la transacción exista, que la fecha sea menor a 24 horas y procesa la anulación restando el precio de la " +
             "transacción a la tarjeta.")
     public ResponseEntity<TransactionManager> annulation(@RequestBody TransactionMapper mapper) {
@@ -48,7 +48,7 @@ public class TransactionControllerImpl implements ITransactionController {
 
     @Override
     @GetMapping
-    @ApiOperation(value = "Consulta todas las transacciones realizadas.")
+    @Operation(summary = "Consulta todas las transacciones realizadas.")
     public ResponseEntity<List<TransactionManager>> findAll() {
         return transactionService.findAll();
     }
