b1e3911f7b124c1b7dd23127e96d78be1978646f
Gustavo Castro <gustavoprasca@hotmail.com>
2025-10-18 09:45:45 -0500

    run insert_users.sql when run sprinboot initial

--- src/main/resources/insert_users.sql
+++ src/main/resources/insert_users.sql
@@ -1,2 +1,8 @@
-insert into customer (password, roles, username) values ('$2y$04$IgiMa8bMxHlHELpTX4FcMeOszUOHL57FGl/Ei/mh9yB4fvRQsORhe', 'ROLE_admin', 'gcastro');
-insert into customer (password, roles, username) values ('$2y$04$Pcs40WPVivgF7GSHwbFSz.fQ0ZiX1Ds5EoyYkth4MYeKi7TOaf5eS', 'ROLE_test', 'test');
+-- Insert initial users for PostgreSQL
+INSERT INTO customer (password, roles, username) VALUES 
+('$2y$04$IgiMa8bMxHlHELpTX4FcMeOszUOHL57FGl/Ei/mh9yB4fvRQsORhe', 'ROLE_admin', 'gcastro')
+ON CONFLICT (username) DO NOTHING;
+
+INSERT INTO customer (password, roles, username) VALUES 
+('$2y$04$Pcs40WPVivgF7GSHwbFSz.fQ0ZiX1Ds5EoyYkth4MYeKi7TOaf5eS', 'ROLE_test', 'test')
+ON CONFLICT (username) DO NOTHING;
